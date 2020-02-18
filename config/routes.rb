e68cef4e33b4273af7a173e13ab16eb9cdc33a7a
Iris van Dijken <irisvdijken@gmail.com>
2020-02-18 11:50:20 +0100

    task model created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'tasks/new', to: 'taks#new', as: :new_tasks
+  post 'tasks', to: 'tasks#create'
+
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show'
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks', to: 'tasks#update'
+
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
